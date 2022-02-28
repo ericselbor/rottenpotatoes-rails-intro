5877267f9d02c1a2a23e7c0d13e9b6150f9735ac
Ubuntu <ubuntu@ip-172-31-83-50.ec2.internal>
2022-02-28 07:00:02 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,12 +15,7 @@ class MoviesController < ApplicationController
       @hilite_release_date = 'hilite'
     end
     
-    @ratings_to_show = params[:ratings].keys
-    @ratings_to_show_hash = Hash[@ratings_to_show.collect {|key| [key, '1']}]
-    
-    @movies = Movie.with_ratings(@ratings_to_show)
-    @movies = @movies.order(params[:sort]) if params[:sort] != ''
-    #@movies = Movie.all.order(@clicked) #sorts by title or release date in ascending order
+    @movies = Movie.all.order(@clicked) #sorts by title or release date in ascending order
   end
 
   def new
