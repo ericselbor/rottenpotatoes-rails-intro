1f2f79f6ca841c5d499ceb2796161b4bc0f020e6
Ubuntu <ubuntu@ip-172-31-83-50.ec2.internal>
2022-03-01 01:02:27 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,13 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         return ['G', 'PG', 'PG-13', 'R']
     end
+    
+    def self.with_ratings(ratings)
+        if ratings != nil
+            rate = ratings.map{|string| string.upcase}
+            return Movie.where(rating: rate)
+        else
+            return Movie.all
+        end
+    end
 end
